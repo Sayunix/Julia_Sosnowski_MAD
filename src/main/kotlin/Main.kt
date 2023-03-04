8dbb65d6a3e865d5c6f93a98eb8b84f9dcb23f6c
Ju <julia.sosnowski@stud.fh-campuswien.ac.at>
2023-03-04 01:43:30 +0100

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,28 @@
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val game = NumberGuessingGame()
+
+    // Generate a random 4-digit number for the user to guess.
+    game.generateRandomNumber()
+
+    // Ask the user to guess the number until they get it right.
+    while (!game.hasUserGuessedNumber()) {
+        print("Enter a 4-digit number with no repeating digits: ")
+        val userGuess = scanner.nextLine()
+
+        // Check if the user's guess is valid.
+        if (userGuess.length != 4 || !game.isValidNumber(userGuess)) {
+            println("Invalid input. Please enter a 4-digit number with no repeating digits.")
+            continue
+        }
+
+        // Calculate the number of digits guessed correctly regardless of their position (n)
+        // and the number of digits guessed correctly at their correct position (m).
+        val (n, m) = game.calculateGuessResult(userGuess)
+        println("$n:$m")
+    }
+
+    println("Congratulations! You guessed the number correctly.")
+}
