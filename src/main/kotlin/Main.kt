4133d82d1749bfef89953fb94d0a9908f95c2625
Ju <julia.sosnowski@stud.fh-campuswien.ac.at>
2023-03-05 17:37:12 +0100

    Bug fix: instead of print println in line 14

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,6 @@
 import java.util.*
 
 fun main() {
-    val scanner = Scanner(System.`in`)
     val game = NumberGuessingGame()
 
     // Generate a random 4-digit number for the user to guess.
@@ -12,7 +11,7 @@ fun main() {
 
     // Ask the user to guess the number until they get it right.
     while (true) {
-        print("Enter a 4-digit number with no repeating digits: ")
+        println("Enter a 4-digit number with no repeating digits: ")
         val userGuess = readLine()
 
         // Check if the user's guess is valid.
