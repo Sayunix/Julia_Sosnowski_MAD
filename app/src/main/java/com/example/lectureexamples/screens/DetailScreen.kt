f849e8b3110e4e8a328607f0c48548a67fe2794c
Ju <julia.sosnowski@stud.fh-campuswien.ac.at>
2023-03-26 22:17:34 +0200

    Code cleaning

--- app/src/main/java/com/example/lectureexamples/screens/DetailScreen.kt
+++ app/src/main/java/com/example/lectureexamples/screens/DetailScreen.kt
@@ -1,22 +1,18 @@
 package com.example.lectureexamples.screens
 
-import androidx.compose.foundation.clickable
+
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
-import coil.compose.rememberImagePainter
 import com.example.lectureexamples.models.Movie
 import com.example.lectureexamples.models.getMovies
 
