afe3f66c54d471c712fa6d3f6f16d20d082e59e3
Ju <julia.sosnowski@stud.fh-campuswien.ac.at>
2023-03-26 18:55:38 +0200

    Task 2b b)	(Re)Use your MovieRow composable inside DetailScreen, to show selected movie information finished

--- app/src/main/java/com/example/lectureexamples/navigation/Navigation.kt
+++ app/src/main/java/com/example/lectureexamples/navigation/Navigation.kt
@@ -1,12 +1,27 @@
 package com.example.lectureexamples.navigation
 
 import android.util.Log
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import coil.compose.AsyncImage
+import com.example.lectureexamples.models.Movie
 import com.example.lectureexamples.screens.DetailScreen
 import com.example.lectureexamples.screens.HomeScreen
 
--- app/src/main/java/com/example/lectureexamples/screens/DetailScreen.kt
+++ app/src/main/java/com/example/lectureexamples/screens/DetailScreen.kt
@@ -1,14 +1,41 @@
 package com.example.lectureexamples.screens
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import com.example.lectureexamples.models.Movie
+import com.example.lectureexamples.models.getMovies
 
+fun getMovieDetails(movieId: String?): Movie{
+    val movies = getMovies()
+    var selectedMovie:Movie = movies[0]
+
+    for (movie in movies){
+        if (movie.id == movieId)
+        {
+            selectedMovie = movie;
+        }
+    }
+    return selectedMovie
+}
 @Composable
 fun DetailScreen(navController: NavController, movieTitle: String) {
+
+    val selectedMovie = getMovieDetails(movieTitle)
 
     /*   movieTitle?.let {
         Text(text = "Hello Detailscreen $movieTitle")
@@ -32,6 +59,10 @@ fun DetailScreen(navController: NavController, movieTitle: String) {
                 }
             }
         )
+        MovieRow(movie=selectedMovie)
+
 
     }
 }
+
+
